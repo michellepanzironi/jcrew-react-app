4fc48f11b2302f1c903be1a898c5d5772a22e794
Michelle Panzironi <michelle.panzironi@gmail.com>
2021-08-10 10:28:43 -0400

    product page

--- jcrew-app/src/App.js
+++ jcrew-app/src/App.js
@@ -7,37 +7,11 @@ import ProductPage from './components/ProductPage.js';
 import * as data from './category.json';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <header>
-          <p>
-            Michelle's J.Crew Take Home Assignment
-          </p>
-        </header>
-
-        <Switch>
-          <Route path="/">
-            <CategoryPage />
-          </Route>
-          <Route path="/:id">
-            <ProductPage />
-          </Route>
-        </Switch>
-
-        
-      </div>
-    </BrowserRouter>
-  );
-}
-
-const CategoryPage = () => {
-
   const [productList, setProductList] = useState([]);
 
   useEffect(() => {
     // 
-      //  I was getting a ReadableObject with no body or json
+    //  I was getting a ReadableObject with no body or json
     // 
     // fetch('http://localhost:8000/', {
     //   method: "GET",
@@ -55,15 +29,35 @@ const CategoryPage = () => {
   }, [])
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', margin: '64px auto' }}>
-      {productList && productList.map(product => {
-        return (
-          <Tile product={product} />
-        )
-      })
-      }
-    </div>
-  )
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <p>
+            Michelle's J.Crew Take Home Assignment
+          </p>
+        </header>
+
+        <Switch>
+          <Route exact path="/">
+            <div style={{ display: 'flex', flexWrap: 'wrap', margin: '64px auto' }}>
+              {productList && productList.map(product => {
+                return (
+                  <Tile product={product} />
+                )
+              })
+              }
+            </div>
+          </Route>
+          <Route path="/:id">
+            <ProductPage productList={productList} />
+          </Route>
+        </Switch>
+
+        
+      </div>
+    </BrowserRouter>
+  );
 }
+
 
 export default App;
--- jcrew-app/src/components/ProductPage.js
+++ jcrew-app/src/components/ProductPage.js
@@ -1,16 +1,26 @@
-import { useParams, Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import Tile from './Tile.js';
 
-const ProductPage = (product) => {
-  const id = useParams();
-  console.log(id);
-  debugger;
+const ProductPage = ({ productList }) => {
+  // there's probably a better way than passing the whole list
+  const productCode = window.location.pathname.split('/')[1];
+  const product = productList.find(product => product['productCode'] === productCode)
+
   return (
     <div>
       <Tile product={product} />
-      <Link to='/' />
+      <div>
+        {product.colors.map(color => {
+          const colorCode = color.colorCode;
+          const imageUrl = "https://www.jcrew.com/s7-img-facade/".concat(`${productCode}_${colorCode}`)
+          return (
+            <img src={imageUrl} style={{ width: '30px', height: 'auto' }} alt={colorCode}/>
+          )
+        })}
+      </div>
+      <Link to='/'>back</Link>
     </div>
   )
 }
 
-export default ProductPage;
+export default withRouter(ProductPage);
